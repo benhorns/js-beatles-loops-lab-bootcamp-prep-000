08a65000728fae294bb75b76e51d3555608e5517
Benjamin Horns <benhorns@gmail.com>
2019-12-24 18:50:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments){
   const array = [];
-
+    for (var i =1; 1<5; i++) {
+      array.push("${musicians} plays ${instruments}")
+    }
   return array
 }
